96e8400a30d4598ea7b855285849bbd0a945aa1b
Junsha111 <shahjunaid999@gmail.com>
2024-05-02 21:00:42 +0530

    Today's Practice

---
+++ script.js
@@ -0,0 +1,23 @@
+let name = "Junaid";
+let age = 24
+let bool = true ;
+let bool1 = false;
+let intb = BigInt('362675578597534897');
+let sym = Symbol("ffsiufywi")
+let student1 = {
+    name : "Junaid",
+    Parentage : "Qayoom",
+    rollNo : 46,
+    age : 24
+}
+console.log(student1.Parentage);
+
+let list = ["sugar" , "vegetables", 500000, true , false  ]
+
+console.log(list[2])
+
+let date = new Date();
+console.log("Date", date.getDate())
+console.log("junaid")
+console.log(student1.Parentage)
+console.log(list[4])
